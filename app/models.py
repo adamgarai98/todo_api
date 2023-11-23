437eafac8a5dec2231ffd15b9be4adc0563f8492
Adam <a.garai98@gmail.com>
2023-11-23 19:05:09 +0000

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class Task:
+    def __init__(self, id, title, description, completed, due_date):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.completed = completed
+        self.due_date = due_date
